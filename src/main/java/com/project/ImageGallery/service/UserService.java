3d2169d01449e75dc8f84ac935f253c087c7d126
Krzysztof Krawczyk <krzysztofskrawczyk@gmail.com>
2023-01-27 22:52:47 +0100

    Added get method for user

--- src/main/java/com/project/ImageGallery/service/UserService.java
+++ src/main/java/com/project/ImageGallery/service/UserService.java
@@ -2,6 +2,7 @@ package com.project.ImageGallery.service;
 
 import com.project.ImageGallery.model.User;
 import com.project.ImageGallery.repository.UserRepository;
+import lombok.Data;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -10,7 +11,10 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.Optional;
+
 @Service
+@Data
 public class UserService implements UserDetailsService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
@@ -31,6 +35,10 @@ public class UserService implements UserDetailsService {
         return userRepository.save(user);
     }
 
+    public User getUser(Long id) {
+        return getUserOrThrowNotFound(id);
+    }
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepository.findByUsername(username);
@@ -38,5 +46,13 @@ public class UserService implements UserDetailsService {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found.");
 
         return user;
+    }
+
+    private User getUserOrThrowNotFound(long id) {
+        Optional<User> targetQuery = userRepository.findById(id);
+        if (targetQuery.isEmpty())
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+
+        return targetQuery.get();
     }
 }
