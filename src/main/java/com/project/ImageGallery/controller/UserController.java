4608628a2f3956a972fb491ffb0706c79de3a6eb
Krzysztof Krawczyk <krzysztofskrawczyk@gmail.com>
2023-02-02 16:36:00 +0100

    Removed redundant tags

--- src/main/java/com/project/ImageGallery/controller/UserController.java
+++ src/main/java/com/project/ImageGallery/controller/UserController.java
@@ -1,6 +1,5 @@
 package com.project.ImageGallery.controller;
 
-import com.project.ImageGallery.model.User;
 import com.project.ImageGallery.schema.LoginResponseSchema;
 import com.project.ImageGallery.schema.MessageResponseSchema;
 import com.project.ImageGallery.schema.UserSchema;
@@ -11,7 +10,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins = "http://localhost:8081", allowedHeaders = "*")
 @RestController
 public class UserController {
     private final UserService userService;
